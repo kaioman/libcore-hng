7b9e7a4f83bcc55ae606540a2197e7277dd5f417
kaioman <kajin0318@gmail.com>
2026-01-24 14:33:01 +0900

    設定ファイル格納ディレクトリ名を環境変数で指定可能とする対応

--- src/libcore_hng/core/base_config.py
+++ src/libcore_hng/core/base_config.py
@@ -17,7 +17,7 @@ class BaseConfig(BaseConfigModel):
     """ プロジェクトルートパス """
     
     @classmethod
-    def load_config(cls: type[T], caller_file: str, *file_names: str, config_dir: Path | None = None) -> T:
+    def load_config(cls: type[T], caller_file: str, *file_names: str, optional_config_dir: Path | None = None) -> T:
         """
         設定ファイルを読み込む
         
@@ -28,23 +28,36 @@ class BaseConfig(BaseConfigModel):
             呼び出し元ファイルの__file__
         file_names : str
             設定ファイル名のか可変長引数
-        config_dir : Path
+        optional_config_dir : Path
             設定ファイルのディレクトリ
             指定時はPathオブジェクトで指定する 例：Path("path/to/configs")
         """
         
-        if config_dir is None:
+        if optional_config_dir is None:
+            
+            # 設定ファイル格納ディレクトリ名を環境変数から取得
+            config_dir_name = "configs"
+            if "CONFIG_DIR_NAME" in os.environ:
+                config_dir_name = os.environ["CONFIG_DIR_NAME"]
+            
             # 環境変数CONFIG_DIRの設定有無を確認
             if "PROJECT_ROOT" in os.environ:
-                # 環境変数より設定ファイル格納パスを取得
+                # 環境変数よりプロジェクトルートパスを取得
                 project_root = Path(os.environ["PROJECT_ROOT"]).resolve()
-                
+            elif "CONFIG_DIR" in os.environ:
+                # 環境変数より設定ファイル格納ディレクトリパスを取得
+                config_dir_env = Path(os.environ["CONFIG_DIR"]).resolve()
+                # プロジェクトルートパスを取得
+                project_root = config_dir_env.parent
             else:
                 # プロジェクトルートパスを取得
                 project_root = find_project_root(Path(caller_file))
 
-        # 設定ファイル格納パスを取得
-        config_dir = project_root / "configs"
+            # 設定ファイル格納パスを取得
+            config_dir = project_root / config_dir_name
+
+        else:
+            config_dir = optional_config_dir
 
         # 設定ファイルを読み込んでマージする
         merged = {}
