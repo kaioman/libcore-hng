44de4cf5ab1afd1dcaef9f049cc10b05e5d7da2d
kaioman <kajin0318@gmail.com>
2026-04-07 01:05:25 +0900

    file_renamerテストコード dry_run=Falseに変更

--- tests/test_025_file_rename.py
+++ tests/test_025_file_rename.py
@@ -12,7 +12,7 @@ backup_dir = "D:\\OneDrive\\02 Picture\\アバター\\LoRA\\Aoi\\Tags\\backup"
 renamer = FileRenamer(target_dir)
 
 print("\n--- Actual Rename (pngファイルのみ、prefix 'aoi_', 連番開始1, バックアップあり) ---") # 変更
-results = renamer.rename_files(prefix="aoi_", extension="png", start_index=1, dry_run=True, backup_directory=backup_dir) # 変更
+results = renamer.rename_files(prefix="aoi_", extension="png", start_index=1, dry_run=False, backup_directory=backup_dir) # 変更
 for res in results:
     print(res)
 
